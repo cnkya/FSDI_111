37fd7aabea70440339fa731d42bdbb172d31005f
Christina Nkya <christina.nkya@sdgku.edu>
2024-08-01 13:48:46 -0400

    changed endpoint to aboutme

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import Flask # from the flask module import the flask calls
 
 app = Flask(__name__)# creates an instance of the flask class (an obj)
 
-@app.get("/") #flask decorator that "wraps"our view function
+@app.get("/aboutme") #flask decorator that "wraps"our view function
 
 def index(): #view function
     me={ #python3 dictionary (key-value pairs)
